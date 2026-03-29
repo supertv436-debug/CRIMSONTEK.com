cb0475e719461284f0ad1979090c6147d6203b01
supertv436-debug <supertv436@gmail.com>
2026-03-29 20:48:57 +0500

    Update crimson-config.js

--- crimson-config.js
+++ crimson-config.js
@@ -11,7 +11,7 @@ const CRIMSON_TG = {
  * Секрет владельца: публикация роликов/фото/GIF только после ввода на admin_media.html.
  * Обязательно смените на свой пароль (иначе любой, кто видит код, сможет войти).
  */
-const CRIMSON_ADMIN_SECRET = 'CHANGE_ME_OWNER_KEY';
+const CRIMSON_ADMIN_SECRET = 'admin0243';
 
 /**
  * Чат с нейросетью (OpenAI-совместимый API).
